8b1f169712516162d596337e9a8e8be4675a40aa
VSebi07 <vsebi0723@gmail.com>
2024-06-23 14:42:42 +0200

    szia henrik
    tudod ezt szerkeszteni?

---
+++ sargaturo/turo.js
@@ -0,0 +1,9 @@
+/*function Mennyisegek(){
+    var tej = document.getElementById("tej");
+    var tojas = document.getElementById("tojas").innerHTML[0];
+    var cukor = document.getElementById("cukor");
+    let szemely = document.getElementById("beviteliMezo").value;
+    alert(szemely)
+}
+
+Mennyisegek()*/
